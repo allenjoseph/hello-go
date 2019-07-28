04ea790d4467258234e9ba2676f385b529796e88
Allen Joseph <allen.joseph.v.a@gmail.com>
2019-07-27 21:40:33 -0500

    feat(server): handle a json response

--- src/server/main.go
+++ src/server/main.go
@@ -1,19 +1,35 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
-type reqHandler struct {
+type rawHello struct {
 	greeting string
 }
 
-func (handler reqHandler) ServeHTTP(resWriter http.ResponseWriter, req *http.Request) {
-	resWriter.Write([]byte(fmt.Sprintf("%v world", handler.greeting)))
+func (handler rawHello) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	res.Write([]byte(fmt.Sprintf("%v world", handler.greeting)))
+}
+
+type jsonHello struct {
+	Greeting string `json:"hello"`
+}
+
+func (handler jsonHello) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	output, err := json.Marshal(handler)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		res.Header().Set("Content-Type", "application/json")
+		res.Write(output)
+	}
 }
 
 func main() {
-	http.Handle("/", &reqHandler{greeting: "hello"})
+	http.Handle("/", &rawHello{greeting: "hello"})
+	http.Handle("/json", &jsonHello{Greeting: "world"})
 	http.ListenAndServe(":8000", nil)
 }
