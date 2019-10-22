ab1c85828fce2994a2aa730e4af7ca9874379717
Allen Joseph <allen.joseph.v.a@gmail.com>
2019-10-21 23:41:24 -0500

    feat(query): add query-service and client to test subscription

---
+++ src/cqrs/client/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/allenjoseph/hello-go/src/cqrs/messaging"
+	"github.com/allenjoseph/hello-go/src/cqrs/util"
+)
+
+func main() {
+	defer messaging.Close()
+
+	// Connect to Nats
+	addrNATS := "nats://nats:4222"
+	es, _ := messaging.OpenConnection(addrNATS)
+	messaging.SetEventStore(es)
+	log.Println("NATS connected")
+
+	woofListener, err := messaging.SubscribeWoofMessage()
+	if err != nil {
+		util.FailOnError(err, "Failed to subscribe to Woof messages")
+	}
+	for woof := range woofListener {
+		fmt.Println(woof)
+	}
+}
--- src/cqrs/util/util.go
+++ src/cqrs/util/util.go
@@ -9,16 +9,16 @@ import (
 
 // ResponseOk util function
 func ResponseOk(w http.ResponseWriter, body interface{}) {
-	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
 
 	json.NewEncoder(w).Encode(body)
 }
 
 // ResponseError util function
 func ResponseError(w http.ResponseWriter, code int, message string) {
-	w.WriteHeader(code)
 	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
 
 	body := map[string]string{
 		"error": message,
